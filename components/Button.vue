1efb2a412506c0f93019053be6e5a1d9439dcfe8
sendilkumarn <sendilkumarn@live.com>
2020-05-02 23:43:55 +0200

    Initial commit

---
+++ components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <a class="ma-3 flex flex-row justify-center" v-bind="$attrs">
+        {{ text }}
+    </a>  
+</template>
+
+<script>
+export default {    
+    props: ['color', 'text'],
+}
+</script>
+
+<style scoped>
+a {
+  border-radius: 4px;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+</style>
---
+++ components/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+    <img src="~static/JHipsterKotlinSticker.svg" class="logo" width="100%" height="100%"/>
+</template>
+
+<style>
+.logo {
+  animation: 3s appear;
+}
+</style>
