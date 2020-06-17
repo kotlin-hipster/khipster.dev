875ba15099a4c32cd65cfcb54a8532aa7aed8bc8
sendilkumarn <sendilkumarn@live.com>
2020-06-18 00:42:21 +0200

    move to docusaurus

---
+++ .docusaurus/registry.js
@@ -0,0 +1,7 @@
+export default {
+  'component---site-src-pages-index-jsc-4-f-f99': [() => import(/* webpackChunkName: 'component---site-src-pages-index-jsc-4-f-f99' */ "@site/src/pages/index.js"), "@site/src/pages/index.js", require.resolveWeak("@site/src/pages/index.js")],
+  'component---theme-doc-item-178-a40': [() => import(/* webpackChunkName: 'component---theme-doc-item-178-a40' */ "@theme/DocItem"), "@theme/DocItem", require.resolveWeak("@theme/DocItem")],
+  'component---theme-doc-page-1-be-9be': [() => import(/* webpackChunkName: 'component---theme-doc-page-1-be-9be' */ "@theme/DocPage"), "@theme/DocPage", require.resolveWeak("@theme/DocPage")],
+  'content---docsc-6-b-c15': [() => import(/* webpackChunkName: 'content---docsc-6-b-c15' */ "@site/docs/gettingStarted.md"), "@site/docs/gettingStarted.md", require.resolveWeak("@site/docs/gettingStarted.md")],
+  'docsMetadata---docs-098-944': [() => import(/* webpackChunkName: 'docsMetadata---docs-098-944' */ "~docs/site-docs-getting-started-md-c6b-base.json"), "~docs/site-docs-getting-started-md-c6b-base.json", require.resolveWeak("~docs/site-docs-getting-started-md-c6b-base.json")],
+  'docsMetadata---docs-route-20-a-f24': [() => import(/* webpackChunkName: 'docsMetadata---docs-route-20-a-f24' */ "~docs/docs-route-ff2.json"), "~docs/docs-route-ff2.json", require.resolveWeak("~docs/docs-route-ff2.json")],};
---
+++ docusaurus.config.js
@@ -0,0 +1,99 @@
+module.exports = {
+  title: 'KHipster',
+  tagline: 'Set up a modern Full Stack web app by running a single command.',
+  url: 'https://khipster.dev',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'sendilkumarn',
+  projectName: 'khipster.dev',
+  themeConfig: {
+    announcementBar: {
+      id: 'supportus',
+      content:
+        '⭐️ If you like KHipster, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/jhipster/jhipster-kotlin">GitHub</a>! ⭐️',
+    },
+    navbar: {
+      title: 'KHipster',
+      logo: {
+        alt: 'KHipster Logo',
+        src: 'img/khip.svg',
+      },
+      links: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        // {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/jhipster/jhipster-kotlin',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/khipster',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/sendilkumarn',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              href: 'https://dev.to/sendilkumarn/kotlin-hipster-spring-boot-kotlin-4956',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/jhipster/jhipster-kotlin',
+            },
+          ],
+        },
+        {
+          title: 'Contributions',
+          items: [
+            {
+              label: 'GitHub Sponsors',
+              href: 'https://github.com/sponsors/sendilkumarn',
+            },
+            {
+              label: 'Open Collective',
+              href: 'https://opencollective.com/generator-jhipster',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Sendilkumarn. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'gettingStarted',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/sendilkumarn/khipster.dev/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
