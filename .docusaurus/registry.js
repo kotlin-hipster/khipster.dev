d6c1b96c8bf0fea36b1eeb68f33dc8afbaef90fd
sendilkumarn <sendilkumarn@live.com>
2020-06-21 00:07:07 +0200

    feat: clean up things

--- .docusaurus/registry.js
+++
@@ -1,7 +0,0 @@
-export default {
-  '098a5b1b': [() => import(/* webpackChunkName: '098a5b1b' */ "~docs/site-docs-getting-started-md-c6b-base.json"), "~docs/site-docs-getting-started-md-c6b-base.json", require.resolveWeak("~docs/site-docs-getting-started-md-c6b-base.json")],
-  '17896441': [() => import(/* webpackChunkName: '17896441' */ "@theme/DocItem"), "@theme/DocItem", require.resolveWeak("@theme/DocItem")],
-  '1be78505': [() => import(/* webpackChunkName: '1be78505' */ "@theme/DocPage"), "@theme/DocPage", require.resolveWeak("@theme/DocPage")],
-  '20ac7829': [() => import(/* webpackChunkName: '20ac7829' */ "~docs/docs-route-ff2.json"), "~docs/docs-route-ff2.json", require.resolveWeak("~docs/docs-route-ff2.json")],
-  'c4f5d8e4': [() => import(/* webpackChunkName: 'c4f5d8e4' */ "@site/src/pages/index.js"), "@site/src/pages/index.js", require.resolveWeak("@site/src/pages/index.js")],
-  'c6bd7e46': [() => import(/* webpackChunkName: 'c6bd7e46' */ "@site/docs/gettingStarted.md"), "@site/docs/gettingStarted.md", require.resolveWeak("@site/docs/gettingStarted.md")],};
